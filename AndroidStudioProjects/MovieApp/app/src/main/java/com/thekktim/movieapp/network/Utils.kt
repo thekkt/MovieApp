a572a95776fa441d0a73eea5ab627bc28488adc1
thekkt <79354241+thekkt@users.noreply.github.com>
2021-06-19 16:10:08 +0600

    Update Utils.kt

--- AndroidStudioProjects/MovieApp/app/src/main/java/com/thekktim/movieapp/network/Utils.kt
+++ AndroidStudioProjects/MovieApp/app/src/main/java/com/thekktim/movieapp/network/Utils.kt
@@ -4,5 +4,5 @@ package com.thekktim.movieapp.network
 object Utils {
     const val BASE_URL = "https://api.themoviedb.org/3/discover/"
     const val IMAGE_URL = "https://image.tmdb.org/t/p/w500/"
-    const val API_KEY = "87c734e18c18abdd53d3fda66f5cd1da"
+    const val API_KEY = "1df5066446f40917f764894077d9ed6f"
 }
